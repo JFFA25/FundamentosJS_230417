6c1a265bd69f8bb8fed439075aefc52efeebd267
JFFA25 <230417@utxicotepec.edu.mx>
2024-09-28 11:42:57 -0600

    Practica04
    Tipos de datos en JS pt2 : String , BigInit , Symbol

--- JS/Ejercicio02.js
+++ JS/Ejercicio02.js
@@ -58,4 +58,84 @@ saldo_cuenta= saldo_cuenta - (cantidad*costo_producto);
 console.log(`Tu nuevo saldo es de: ${saldo_cuenta}`);
 // El cliente realiza un abono de 1500
 monto_transaccion=1500;
+
 console.log(`Tu abono de : ${monto_transaccion} ha sido recibido, tu nuevo saldo es de: ${(saldo_cuenta+monto_transaccion)}`);
+// 4.STRING (Cadena de Caractéres)
+const alumno = "Jose Francisco Flores Amador"
+let producto;
+
+console.warn("--- Tipo de Dato - STRING (Cadena de Caractéres");
+console.log(`El nombre del alumno es: ${alumno}, que es un tipo de dato ${typeof(alumno)}`);
+producto="MONITOR 20\"";
+console.log(`El nombre del producto es: ${producto}, que es un tipo de dato ${typeof(producto)}`);
+
+// Manipulando los STRINGS
+console.log(`Más adelante podremos transformar el contenido de los STRINGS usando funciones específicas cómo convertir a mayúsculas: ${alumno} => ${alumno.toUpperCase()}`);
+console.log(`O en su defecto a minúsculas: ${producto} => ${producto.toLowerCase()}`);
+
+
+
+// 5.BIGINT (Enteros de Dimensiones Amplias) - Generalmente utilizado en sistemas matemáticos, físicos o espaciales donde se requiere de una excelente precisión numérica.
+console.warn("--- Tipo de Dato - BIGINT (Número Ampplio");
+let numeroGrande = 1234567890
+let numeroGrande2 = 12345678901234567890
+let numeroGrande3 = 12345678901234567890123456789
+let numeroGrande4 = 123456789012345678901234567891234567890
+
+console.log(`El primero experimento de un numero grandre es: ${numeroGrande}, que si es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande)}`)
+console.log(`El primero experimento de un numero grandre es: ${numeroGrande2}, que si es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande2)}`)
+console.log(`El primero experimento de un numero grandre es: ${numeroGrande3}, que si es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande3)}`)
+console.log(`El primero experimento de un numero grandre es: ${numeroGrande4}, que si es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande4)}`)
+
+// Para definir el tipo de dato BigInit debemos invocar la funcion BigInit o usar la letra "n" al final del numero o la asignacion de este
+
+console.warn("--Declaramos los valores numericos en gran tamaño--")
+numeroGrande = BigInt(5422021172919176764511184)
+numeroGrande4 = 5422021172919176764511184n
+console.log(`El primero experimento de un numero grandre es: ${numeroGrande}, que si es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande)}`)
+console.log(`El primero experimento de un numero grandre es: ${numeroGrande4}, que si es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande4)}`)
+
+// 6. SYMBOL
+console.warn("--Tipo de Dato : SYMBOL--")
+const numero1 = "2";
+const numero2 = "2.0";
+const numero3 = "2";
+const numero4 = "2.0";
+const numero5 = Symbol(2);
+const numero6 = Symbol(2.0);
+const numero7 = Symbol("2");
+const numero8 = Symbol("2.0");
+
+console.log("Prueba de comparacion 1")
+if (numero1==numero2){
+    console.log(`Se han comparado los valores de numer1 y numero2, confirmo que tienen el mismo valor`)
+}
+else{
+    console.log(`Se han comparado los valores de numer1 y numero2, detectando que No tienen el mismo valor`)
+}
+console.log("Prueba de comparacion 1 : ¿ es 2 = 2?")
+if (numero1==numero3){
+    console.log(`Se han comparado los valores de numer1 y numero2, confirmo que tienen el mismo valor`)
+}
+else{
+    console.log(`Se han comparado los valores de numer1 y numero3, detectando que No tienen el mismo valor`)
+}
+
+// Prueba de comparacion 3 : ¿Es 2 = "3"?
+console.log("Comparacion  3 : ¿Es 2 = 3")
+if (numero1===numero3){
+    console.log(`Se han comparado los valores de numer1 y numero3, confirmo que tienen el mismo valor`)
+}
+else{
+    console.log(`Se han comparado los valores de numer1 y numero3, detectando que No tienen el mismo valor`)
+}
+// Prueba de comparacion 4 : ¿ es 2 = "4.0"?
+console.log("Prueba de comparacion 4 : ¿ es 2 = 4.0")
+if (numero4 === numero2){
+    console.log(`Se han comparado los valores de numer1 y numero4, confirmo que tienen el mismo valor`)
+}
+else{
+    console.log(`Se han comparado los valores de numer1 y numero4, detectando que No tienen el mismo valor`)
+}
+// Prueba de comparacion 5 : ¿
+//console.log()
